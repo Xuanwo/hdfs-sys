774201920d377d93195b9e06b3dbe92beb6efc04
Xuanwo <github@xuanwo.io>
2023-07-11 21:26:02 +0800

    remove native
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- build.rs
+++ build.rs
@@ -11,12 +11,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // I don't know where to find libjvm.so exactly.
     // The only thing I can do is search everywhere I know.
-    println!("cargo:rustc-link-search=native={java_home}/bin/client");
-    println!("cargo:rustc-link-search=native={java_home}/bin/server");
-    println!("cargo:rustc-link-search=native={java_home}/lib/server");
-    println!("cargo:rustc-link-search=native={java_home}/lib/amd64/server");
-    println!("cargo:rustc-link-search=native={java_home}/jre/lib/server");
-    println!("cargo:rustc-link-search=native={java_home}/jre/lib/amd64/server");
+    println!("cargo:rustc-link-search={java_home}/bin/client");
+    println!("cargo:rustc-link-search={java_home}/bin/server");
+    println!("cargo:rustc-link-search={java_home}/lib/server");
+    println!("cargo:rustc-link-search={java_home}/lib/amd64/server");
+    println!("cargo:rustc-link-search={java_home}/jre/lib/server");
+    println!("cargo:rustc-link-search={java_home}/jre/lib/amd64/server");
     println!("cargo:rustc-link-lib=jvm");
 
     // Static link compiled `libhdfs.a`
